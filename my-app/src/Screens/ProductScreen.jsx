f6b751ca6ea6905e25b0e451d4b87e0fd5c8bf8d
NjohPrince <jufredprince@gmail.com>
2020-12-10 08:34:09 +0100

    confirmed access using the product id

--- my-app/src/Screens/ProductScreen.jsx
+++ my-app/src/Screens/ProductScreen.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Data from "../Data"
 
 function ProductScreen(props) {
-  console.log(props.match.params.id)
-  return <div>TestDirect</div>;
+  const product = Data.products.find(x => x._id === props.match.params.id);
+  return <div>{product.name}</div>;
 }
 
 export default ProductScreen;
